908762a3ed91ee59f2e8609230bd1a41a565636f
ChunJeong <hun4742@gmail.com>
2023-07-23 00:09:55 +0900

    리스트 3.2 FlowController의 코드 작성

--- FlowController.hpp
+++ FlowController.hpp
@@ -3,19 +3,46 @@
 class FileDataReader
 {
 public:
-    std::byte read()
+    std::byte* read()
     {
 
     }
+};
 
+class Encryptor
+{
+public:
+    std::byte* encrypt(std::byte* data)
+    {
+
+    }
+};
+
+class FileDataWriter
+{
+public:
+    void write(std::byte* data)
+    {
+
+    }
 };
 
 class FlowController
 {
+public:
     void process()
     {
         FileDataReader* reader = new FileDataReader();
-        std::byte data = reader->read();
+        std::byte* data = reader->read();
 
+        Encryptor* encryptor = new Encryptor();
+        std::byte* encryptedData = encryptor->encrypt(data);
+
+        FileDataWriter* writer = new FileDataWriter();
+        writer->write(encryptedData);
+
+        delete reader;
+        delete encryptor;
+        delete writer;
     }
 };
